164f7d01d050491da2a372c71c25b4a574c08354
Fyced <96468855+Fyced@users.noreply.github.com>
2023-09-25 20:53:42 +0200

    Create horario.sh

---
+++ horario.sh
@@ -0,0 +1,12 @@
+RED="\e[31m"
+
+echo -e "${RED}Horas   Lunes	  Martes     Miercoles	    Jueves      Viernes"
+echo "8:15	Lengua	  Lengua     E.F.	    Valen	Ingles"
+echo "9:10 	Valen	  Dibujo     Ingles	    Tutoria     E.F."
+echo "10:05	Fisica	  Valen	     Dibujo	    Mates	Dibujo"
+echo "11:00   _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-"
+echo "11:25 	E.F.	  Fisica     Filo	    E.F.	Historia"
+echo "12:20	Dibujo    Historia   Mates	    Fisica	Fisica"
+echo "13:15   _-_-_-_-_-_-_-_-_-   Historia	    Filo	Lengua"
+echo "13:30	Mates	  Mates"
+echo "14:25	Filo	  Ingles"
